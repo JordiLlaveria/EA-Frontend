05239d185c06222d7d49a91657bd4373824dadd1
JoanaOP <joana.orpella@gmail.com>
2022-05-15 11:15:58 +0200

    Form

--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:frontend/screens/profile_screen.dart';
-import 'package:frontend/screens/search_screen.dart';
-import 'package:frontend/screens/xat_screen.dart';
-import 'package:frontend/screens/home_screen.dart';
+import 'package:frontend/screens/register_screen.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
+void main(){
+  runApp(MyApp());
 }
 
-class _MyAppState extends State<MyApp> {
-  int currentScreen = 0;
-
-  List<Widget> screens = [
-    HomeScreen(),
-    XatScreen(),
-    SearchScreen(),
-    ProfileScreen(),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        title: 'Flutter Grup 1',
-        home: Scaffold(
-          body: screens[currentScreen],
-
-          // Bottom Navigation Bar
-          bottomNavigationBar: BottomNavigationBar(
-            currentIndex: currentScreen,
-            onTap: (index) {
-              setState(() {
-                currentScreen = index;
-              });
-            },
-            type: BottomNavigationBarType.fixed,
-            backgroundColor: Colors.white,
-            unselectedItemColor: Colors.black.withOpacity(0.5),
-            selectedItemColor: Colors.blue,
-            items: [
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.home_filled), label: 'Home'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.chat_rounded), label: 'Xat'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.people_rounded), label: 'Search'),
-              BottomNavigationBarItem(
-                  icon: Icon(Icons.person), label: 'Profile'),
-            ],
-          ),
-        ));
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Grup 1',
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: const RegisterScreen(),
+    );
   }
 }
