42c677a648bdc1288d585b6aca9539c6edaf537e
JordiLlaveria <jordi.llaveria@estudiantat.upc.edu>
2022-05-26 13:34:22 +0200

    Merge improved

--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -3,8 +3,9 @@ import 'dart:developer';
 import 'package:frontend/models/user_model.dart';
 import 'package:http/http.dart' as http;
 
-class UserService{
-  static const apiURL = String.fromEnvironment('API_URL', defaultValue: 'https://ea1-backend.mooo.com');
+class UserService {
+  static const apiURL = String.fromEnvironment('API_URL',
+      defaultValue: 'https://ea1-backend.mooo.com');
   static var baseurl = apiURL + "/api/users";
 
   static Future<User> getUserByName(String name) async {
@@ -24,7 +25,7 @@ class UserService{
     }
     return [];
   }
-}
+
   static Future<User> getUserByID(String id) async {
     var res = await http.get(Uri.parse(baseurl + '/byID/' + id));
     var decoded = jsonDecode(res.body);
