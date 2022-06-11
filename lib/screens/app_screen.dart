3319141693b4639b1882f90dc9101f04571c0eca
mupikga <mg.puig13@gmail.com>
2022-06-11 19:56:54 +0200

    Mínim2

--- lib/screens/app_screen.dart
+++ lib/screens/app_screen.dart
@@ -7,7 +7,6 @@ import 'package:frontend/screens/home_screen.dart';
 import 'package:localstorage/localstorage.dart';
 import 'package:tutorial_coach_mark/tutorial_coach_mark.dart';
 
-
 class AppScreen extends StatefulWidget {
   /* const AppScreen({Key? key}) : super(key: key); */
   @override
@@ -34,68 +33,64 @@ class _AppScreenState extends State<AppScreen> {
 
   List<TargetFocus> targets = <TargetFocus>[];
 
-    late TutorialCoachMark tutorialCoachMark;
+  late TutorialCoachMark tutorialCoachMark;
 
-    void initState(){
-      initTarget();
-      WidgetsBinding.instance?.addPostFrameCallback(_afterlayaout);
-      super.initState();
-    }
+  void initState() {
+    initTarget();
+    WidgetsBinding.instance.addPostFrameCallback(_afterlayaout);
+    super.initState();
+  }
 
-    void _afterlayaout (_){
-      Future.delayed(Duration(milliseconds:100));
-      showTutorial();
-    }
+  void _afterlayaout(_) {
+    Future.delayed(Duration(milliseconds: 100));
+    showTutorial();
+  }
 
-    void showTutorial(){
-      tutorialCoachMark = TutorialCoachMark(
-        context,
-        targets: targets,
-        alignSkip: Alignment.topRight,
-        colorShadow: Theme.of(context).cardColor,
-        opacityShadow: 0.95, 
-      )..show();
-    }
+  void showTutorial() {
+    tutorialCoachMark = TutorialCoachMark(
+      context,
+      targets: targets,
+      alignSkip: Alignment.topRight,
+      colorShadow: Theme.of(context).cardColor,
+      opacityShadow: 0.95,
+    )..show();
+  }
 
-    void initTarget(){
-      targets.add(
-        TargetFocus(
-            identify: "Navigation",
-            keyTarget: keyNavigation,
-            shape: ShapeLightFocus.RRect,
-            radius: 20,
-            enableOverlayTab: true,
-            contents: [
-              TargetContent(
-                  align: ContentAlign.top,
-                  child: Container(
-                    child:Column(
-                      mainAxisSize: MainAxisSize.min,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          "WELCOME TO XERRA",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Colors.black54,
-                            fontSize: 30.0,
-                            fontFamily: 'FredokaOne'                            
-                          ),
-                        ),
-                        Text("With the bottom bar you can navigate through the app.",
-                            style: TextStyle(
-                                color: Colors.redAccent,
-                                fontSize: 20,
-                            ),),
-                      ],
+  void initTarget() {
+    targets.add(TargetFocus(
+        identify: "Navigation",
+        keyTarget: keyNavigation,
+        shape: ShapeLightFocus.RRect,
+        radius: 20,
+        enableOverlayTab: true,
+        contents: [
+          TargetContent(
+              align: ContentAlign.top,
+              child: Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      "WELCOME TO XERRA",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black54,
+                          fontSize: 30.0,
+                          fontFamily: 'FredokaOne'),
                     ),
-                  )
-              )
-            ]
-        )
-    );
-    
-    }
+                    Text(
+                      "With the bottom bar you can navigate through the app.",
+                      style: TextStyle(
+                        color: Colors.redAccent,
+                        fontSize: 20,
+                      ),
+                    ),
+                  ],
+                ),
+              ))
+        ]));
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -119,9 +114,11 @@ class _AppScreenState extends State<AppScreen> {
             unselectedItemColor: Colors.black.withOpacity(0.5),
             selectedItemColor: Colors.blue,
             items: [
-              BottomNavigationBarItem(                                  
-                  icon: Icon(Icons.home_filled), label: 'Home',),
-              BottomNavigationBarItem(                  
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home_filled),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
                   icon: Icon(Icons.chat_rounded), label: 'Chat'),
               BottomNavigationBarItem(
                   icon: Icon(Icons.people_rounded), label: 'Search'),
